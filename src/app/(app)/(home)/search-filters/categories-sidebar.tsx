fb62b0be788f0ab4ea2e6bc3b00018c6df56dec6
zerios99 <zainyasser31@gmail.com>
2025-07-28 19:21:50 +0300

    07-trpc-integrationon

--- src/app/(app)/(home)/search-filters/categories-sidebar.tsx
+++ src/app/(app)/(home)/search-filters/categories-sidebar.tsx
@@ -38,7 +38,9 @@ export const CategoriesSidebar = ({ open, onOpenChange }: Props) => {
 
   const handleCategoryClick = (category: CategoriesGetManyOutput[1]) => {
     if (category.subcategories && category.subcategories.length > 0) {
-      setParentCategories(category.subcategories as CategoriesGetManyOutput);
+      setParentCategories(
+        category.subcategories as unknown as CategoriesGetManyOutput
+      );
       setSelectedCategory(category);
     } else {
       if (parentCategories && selectedCategory) {
--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,6 +1,7 @@
 import { initTRPC } from "@trpc/server";
 import { getPayload } from "payload";
 import config from "@payload-config";
+import superjson from "superjson";
 import { cache } from "react";
 export const createTRPCContext = cache(async () => {
   /**
@@ -16,7 +17,7 @@ const t = initTRPC.create({
   /**
    * @see https://trpc.io/docs/server/data-transformers
    */
-  // transformer: superjson,
+  transformer: superjson,
 });
 // Base router and procedure helpers
 export const createTRPCRouter = t.router;
