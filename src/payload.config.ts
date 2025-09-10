985a4a235f09feb16c8440f9c847ae8528e8df0b
zerios99 <zainyasser31@gmail.com>
2025-09-10 20:00:35 +0300

    storage

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 import { buildConfig } from "payload";
 import { fileURLToPath } from "url";
 import sharp from "sharp";
+import { vercelBlobStorage } from "@payloadcms/storage-vercel-blob";
 
 import { Users } from "./collections/Users";
 import { Media } from "./collections/Media";
@@ -62,6 +63,13 @@ export default buildConfig({
         includeDefaultField: false,
       },
       userHasAccessToAllTenants: (user) => isSuperAdmin(user),
+    }),
+    vercelBlobStorage({
+      enabled: true,
+      collections: {
+        media: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN,
     }),
   ],
 });
